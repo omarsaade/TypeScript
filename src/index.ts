be981533a69498612a51495099fe58ac4fe7c263
omar saade <omarsaadeh11@gmail.com>
2022-09-19 18:03:21 +0300

    TypeScript

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+console.log(Math.floor(10.5)); // create TS File
+//the comment will not show
---
+++ src/test.ts
@@ -0,0 +1,31 @@
+// console.log(Math.round(10.6)); // create TS File
+let theName: string = "Elzero";
+let theAge: number = 40;
+//         type annotations
+let hire: boolean = true;
+
+theName = "osama";
+
+// iza jeyik data w mesh 3aref el type taba3a shu bta3mul?
+
+// Answer
+let all: any = "elzero web school";
+all = 100;
+//fik ta3mul update 3ade w mafi mechkle.
+
+//or
+// Answer
+let allo: any;
+allo = 100;
+//fik ta3mul update 3ade w mafi mechkle.
+let allVars; //Any
+
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+// console.log(add(10, 20));
+// console.log(typeof add(10, 20));
+
+console.log(add(10, 20));
+console.log(typeof add(10, 20));
